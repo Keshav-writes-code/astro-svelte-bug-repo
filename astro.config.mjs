bcd6fd53b8696d320b4db8ac1a34803090802075
Keshav <95571677+Keshav-writes-code@users.noreply.github.com>
2025-03-26 07:22:27 +0530

    fix(deploy): add base param for resource loading

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import svelte from '@astrojs/svelte';
+import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [svelte()]
+  integrations: [svelte()],
+  site: "https://Keshav-writes-code.github.io",
+  base: "astro-svelte-bug-repo",
 });
+
