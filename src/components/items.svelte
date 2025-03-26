ea18dd79623f490e41685c20b463d26dc1fb7444
Keshav <95571677+Keshav-writes-code@users.noreply.github.com>
2025-03-26 06:35:56 +0530

    feat(ui): add items component

---
+++ src/components/items.svelte
@@ -0,0 +1,55 @@
+<script lang="ts">
+  type Props = {
+    items: {
+      name: string;
+    }[];
+  };
+  let { items }: Props = $props();
+  let items_mutable = items;
+  $inspect(items);
+  $inspect(items_mutable);
+</script>
+
+<div>
+  {#each items_mutable as item}
+    <input bind:value={item.name} type="text" />
+  {/each}
+</div>
+<div>
+  <table>
+    <caption>Items : </caption>
+    <tbody>
+      {#each items as item, i}
+        <tr>
+          <td>{i} </td>
+          <td>{item.name} </td>
+        </tr>
+      {/each}
+    </tbody>
+  </table>
+
+  <table>
+    <caption>Items_mutable : </caption>
+    <tbody>
+      {#each items_mutable as item, i}
+        <tr>
+          <td>{i} </td>
+          <td>{item.name} </td>
+        </tr>
+      {/each}
+    </tbody>
+  </table>
+</div>
+
+<style>
+  table,
+  td {
+    border: 1px solid black;
+    border-collapse: collapse;
+    padding: 1rem;
+  }
+  div {
+    display: flex;
+    gap: 2rem;
+  }
+</style>
